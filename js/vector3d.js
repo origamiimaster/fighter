1daa413fd1fb223fb3cde9c3cf7d2fa6852480bf
Sebastian Clancy <foldit.com@gmail.com>
2021-01-08 21:15:08 -0800

    Started Vec3 for this idea

---
+++ js/vector3d.js
@@ -0,0 +1,9 @@
+class Vector3D {
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+export { Vector3D };
+exports.Vector3D = Vector3D;
---
+++ js/vector3d.ts
@@ -0,0 +1,13 @@
+class Vector3D {
+    x: number;
+    y: number;
+    z: number;
+    constructor(x: number, y: number, z: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+export { Vector3D }
+
+exports.Vector3D = Vector3D
